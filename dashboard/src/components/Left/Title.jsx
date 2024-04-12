c291ac2592d3bd921cb75b81ba6498906f53e983
Jackall05 <156783166+Jackall05@users.noreply.github.com>
2024-04-12 22:22:28 +0200

    cambio del nome del sito

--- dashboard/src/components/Left/Title.jsx
+++ dashboard/src/components/Left/Title.jsx
@@ -6,7 +6,7 @@ function Title() {
     <div className="boxy">
       <div className="title-header">
         <img src="Logo_Basket.png"  alt="Basketball" className="title-image" />
-        <h1 className="title-heading">Magic Balls</h1>
+        <h1 className="title-heading">Slum Dunk Stats</h1> 
       </div>
       <p className="subtitle">Here you will find all the results of the matches. Enjoy!</p>
     </div>
